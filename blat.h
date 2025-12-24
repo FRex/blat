be466c7db8e0f526ed4b54794b1a0de47378ba48
FRex <blazej.roszkowski@gmail.com>
2025-12-24 19:51:17 +0100

    add linux implementation

--- blat.h
+++ blat.h
@@ -7,7 +7,7 @@ long long blat_microseconds(void);
 
 void blat_sleepseconds(double seconds);
 void blat_sleepmilliseconds(double milliseconds);
-void blat_sleepmicroseconds(double microseconds);
+void blat_sleepmicroseconds(long long microseconds);
 
 #endif /* BLAT_H_FILE_INCLUDED */
 
@@ -47,8 +47,49 @@ long long blat_microseconds(void)
 /* TODO: consider using timeGetDevCaps then doing timeBeginPeriod/timeEndPeriod with wPeriodMin */
 void blat_sleepseconds(double seconds) {Sleep((DWORD)(seconds * 1000.0));}
 void blat_sleepmilliseconds(double milliseconds) {Sleep((DWORD)milliseconds);}
-void blat_sleepmicroseconds(double microseconds) {Sleep((DWORD)(microseconds / 1000.0));}
+void blat_sleepmicroseconds(long long microseconds) {Sleep((DWORD)(microseconds / 1000));}
 
 #endif /* _WIN32 */
+
+#ifdef __linux__
+
+#include <time.h> /* for nanosleep, clock_gettime */
+
+double blat_seconds(void)
+{
+    struct timespec ts;
+    /* TODO: consider using CLOCK_MONOTONIC outside */
+    clock_gettime(CLOCK_MONOTONIC_RAW , &ts);
+    return ts.tv_sec + ts.tv_nsec / 1000000000.0;
+}
+
+double blat_milliseconds(void)
+{
+    struct timespec ts;
+    /* TODO: consider using CLOCK_MONOTONIC outside */
+    clock_gettime(CLOCK_MONOTONIC_RAW , &ts);
+    return 1000.0 * ts.tv_sec + ts.tv_nsec / 100000.0;
+}
+
+long long blat_microseconds(void)
+{
+    struct timespec ts;
+    /* TODO: consider using CLOCK_MONOTONIC outside */
+    clock_gettime(CLOCK_MONOTONIC_RAW , &ts);
+    return 1000000ll * ts.tv_sec + ts.tv_nsec / 1000;
+}
+
+void blat_sleepseconds(double seconds)
+{
+    struct timespec ts;
+    ts.tv_sec = seconds;
+    ts.tv_nsec = 1000000000ll * (seconds - ts.tv_sec);
+    nanosleep(&ts, NULL);
+}
+
+void blat_sleepmilliseconds(double milliseconds) {blat_sleepseconds(milliseconds / 1000.0);}
+void blat_sleepmicroseconds(long long microseconds) {blat_sleepseconds(microseconds / 1000000.0);}
+
+#endif /* __linux__ */
 
 #endif /* BLAT_DO_IMPLEMENTATION */
