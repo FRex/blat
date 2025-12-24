2102a600e2d8f6504155a88c8766cc7c54d256a5
FRex <blazej.roszkowski@gmail.com>
2025-12-24 18:19:55 +0100

    add `_priv_` in the name of private variable

--- blat.h
+++ blat.h
@@ -16,22 +16,22 @@ long long blat_microsecondsdelta(long long * start);
 #include <profileapi.h> /* for QueryPerformanceFrequency, QueryPerformanceCounter */
 #include <synchapi.h> /* for Sleep */
 
-static LARGE_INTEGER blat_gs_frequency;
+static LARGE_INTEGER blat_priv_gs_frequency;
 
 double blat_seconds(void)
 {
     LARGE_INTEGER ret;
-    if(blat_gs_frequency.QuadPart == 0) QueryPerformanceFrequency(&blat_gs_frequency);
+    if(blat_priv_gs_frequency.QuadPart == 0) QueryPerformanceFrequency(&blat_priv_gs_frequency);
     QueryPerformanceCounter(&ret);
-    return ret.QuadPart / (double)blat_gs_frequency.QuadPart;
+    return ret.QuadPart / (double)blat_priv_gs_frequency.QuadPart;
 }
 
 long long blat_microseconds(void)
 {
     LARGE_INTEGER ret;
-    if(blat_gs_frequency.QuadPart == 0) QueryPerformanceFrequency(&blat_gs_frequency);
+    if(blat_priv_gs_frequency.QuadPart == 0) QueryPerformanceFrequency(&blat_priv_gs_frequency);
     QueryPerformanceCounter(&ret);
-    return (1000 * 1000 * ret.QuadPart) / blat_gs_frequency.QuadPart;
+    return (1000 * 1000 * ret.QuadPart) / blat_priv_gs_frequency.QuadPart;
 }
 
 #endif /* _WIN32 */
